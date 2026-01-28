3a223aba4789baa8fcc8380745522df78a9174dd
Sergio Cordoba <sergiocordoba246@gmail.com>
2026-01-28 15:34:24 +0000

    Terminando

--- src/front/pages/Singup.jsx
+++ src/front/pages/Singup.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 
 export const Singup = () => {
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [passwordEye, setPasswordEye] = useState(false)
-
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+  const [passwordEye, setPasswordEye] = useState("")
 
 
   const handleSubmit = (event) => {
@@ -14,15 +15,31 @@ export const Singup = () => {
 
   }
 
+  const handleChange = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+
+    setFormData((gigante)=>{
+      return{...gigante, [name] : value}
+    })
+  }
+
   return (
-    <div>
-      <form className="w-50 mx-auto text-center mt-5" onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input value={email} type={`${passwordEye ? "text" : "password"}`} required></input>
 
-
-
-      </form>      
+    <div className="w-50 mx-auto text-center mt-5">
+      <form className="w-50 mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+          <input onChange={handleChange} name="email" value={formData.email} type="text" className="form-control" id="exampleFormControlInput1"></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Password</label>
+          <input onChange={handleChange} name="password" value={formData.password} type={`${passwordEye ? "text" : "password"}`} className="form-control" id="exampleFormControlTextarea1"></input>
+          <button className="mt-1" onClick={() => setPasswordEye(prev => !prev)}><i className="fa-regular fa-eye"></i></button>
+        </div>
+        <button type="submit" className="btn btn-warning mb-2">Signup</button>
+        <p>You have an account? <Link to="/">Login</Link></p>
+      </form>
     </div>
-  );
+  )
 }
